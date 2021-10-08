7627e508b3664d163439023c5ef890af79028819
maslov.o <maslovoleg.by@gmail.com>
2021-10-08 20:07:12 +0300

    create card

---
+++ quiz/src/App.tsx
@@ -0,0 +1,9 @@
+import { QuizCard } from "./components/quizcard";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <QuizCard />
+    </div>
+  );
+};
---
+++ quiz/src/components/quizcard/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.css";
+
+export const QuizCard = () => {
+  return <div className={styles.quizCard}>Card</div>;
+};
