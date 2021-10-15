64790f9dc1d41f2e5a9d80c9105e9121a4f2b9bd
maslov.o <maslovoleg.by@gmail.com>
2021-10-15 16:32:26 +0300

    add api to the project

--- src/components/quizcard/index.tsx
+++ src/components/quizcard/index.tsx
@@ -1,5 +1,21 @@
+import React, { useEffect } from "react";
 import styles from "./style.module.css";
 
-export const QuizCard = () => {
-  return <div className={styles.quizCard}>Card</div>;
+interface IProps {
+  quest?: string;
+}
+
+const getData = async () =>
+  await fetch("https://restcountries.com/v3.1/name/peru")
+    .then((res) => res.json())
+    .then((el) => el);
+
+export const QuizCard: React.FC<IProps> = ({ quest }) => {
+  const farmDataCpountries = getData().then((res) => console.log(res));
+
+  return (
+    <div className={styles.quizCard}>
+      <h3 className={styles.quizCard_title}>{quest ?? "Some quest"}</h3>
+    </div>
+  );
 };
